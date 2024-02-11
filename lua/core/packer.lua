c002536d4a021ee94cce46413e586acc603db5bb
aspherr <huzaifa.syedd@gmail.com>
2024-02-11 15:59:22 +0000

    installed and configured telescope

--- lua/core/packer.lua
+++ lua/core/packer.lua
@@ -1,8 +1,15 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- plugin manager
-  use 'wbthomason/packer.nvim'
+    -- plugin manager
+    use 'wbthomason/packer.nvim'
+    
+    -- telescope (fuzzy finder)
+    use {
+        'nvim-telescope/telescope.nvim', 
+        tag = '0.1.5',
+        requires = { {'nvim-lua/plenary.nvim'} }
+    }
 
 end)
 

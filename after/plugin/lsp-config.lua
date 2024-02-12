1a340cbf169941a636e2ed0b647c23ce27893e2c
aspherr <huzaifa.syedd@gmail.com>
2024-02-12 14:24:25 +0000

    updated python LSP

--- after/plugin/lsp-config.lua
+++ after/plugin/lsp-config.lua
@@ -1,11 +1,11 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {"rust_analyzer", "lua_ls", "clangd", "jdtls", "pyre"}
+    ensure_installed = {"rust_analyzer", "lua_ls", "clangd", "jdtls", "pylyzer"}
 })
 
 require("lspconfig").rust_analyzer.setup {}
 require("lspconfig").lua_ls.setup {}
 require("lspconfig").clangd.setup {}
 require("lspconfig").jdtls.setup {}
-require("lspconfig").pyre.setup {}
+require("lspconfig").pylyzer.setup {}
 

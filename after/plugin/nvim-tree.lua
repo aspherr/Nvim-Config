ba32839a5db634a9c64fdc20c51ff05478726827
aspherr <huzaifa.syedd@gmail.com>
2024-02-13 16:15:29 +0000

    added a 'focus' motion for nvim-tree

--- after/plugin/nvim-tree.lua
+++ after/plugin/nvim-tree.lua
@@ -4,6 +4,7 @@ vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
 vim.keymap.set("n", "<C-n>", ":NvimTreeFindFileToggle<CR>")
+vim.keymap.set("n", "<C-o>", ":NvimTreeFocus<CR>")
 
 require("nvim-tree").setup()
 

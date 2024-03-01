a9985b1d9a2ee8f4cf54787699a1a10a500d0556
aspherr <huzaifa.syedd@gmail.com>
2024-03-01 23:23:51 +0000

    updated color-scheme to TokyoNight

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,16 @@
+return {
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000,
+  opts = {
+    style = "moon",
+    transparent = false,
+    terminal_colors = true,
+
+    styles = {
+      comments = { italic = true },
+      keywords = { italic = true },
+      sidebar = "dark",
+    },
+  },
+}

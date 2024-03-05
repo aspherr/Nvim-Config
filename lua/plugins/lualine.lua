a2f4f03e58a77c68ee3d0ed4bf88bdc3667f797d
aspherr <huzaifa.syedd@gmail.com>
2024-03-05 02:30:31 +0000

    reconfigured lualine structure

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -144,29 +144,6 @@ return {
         end,
       })
 
-      ins_left({
-        function()
-          local msg = "No Active Lsp"
-          local buf_ft = vim.api.nvim_buf_get_option(0, "filetype")
-          local clients = vim.lsp.get_active_clients()
-
-          if next(clients) == nil then
-            return msg
-          end
-
-          for _, client in ipairs(clients) do
-            local filetypes = client.config.filetypes
-            if filetypes and vim.fn.index(filetypes, buf_ft) ~= -1 then
-              return client.name
-            end
-          end
-
-          return msg
-        end,
-        icon = " LSP:",
-        color = { fg = "#ffffff", gui = "bold" },
-      })
-
       ins_right({
         "o:encoding",
         fmt = string.upper,
@@ -179,6 +156,12 @@ return {
         fmt = string.upper,
         icons_enabled = false,
         color = { fg = colors.green, gui = "bold" },
+      })
+
+      ins_right({
+        "filetype",
+        icons_enabled = true,
+        color = { fg = "#ffffff", gui = "bold" },
       })
 
       ins_right({

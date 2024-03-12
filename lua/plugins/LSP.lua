ef9e1d151cf7301e05379b9862250a7ce6646b40
aspherr <huzaifa.syedd@gmail.com>
2024-03-12 04:59:15 +0000

    updated LSP setup + configured LSP snippets

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -1,18 +1,57 @@
 return {
   {
     "williamboman/mason.nvim",
-    opts = function(_, opts)
-      vim.list_extend(opts.ensure_installed, {
-        "stylua",
-        "shellcheck",
-        "typescript-language-server",
-        "css-lsp",
-        "pyright",
-        "clangd",
-        "shfmt",
-        "rust-analyzer",
-        "jdtls",
-        "lua-language-server",
+    lazy = false,
+    config = function()
+      require("mason").setup()
+    end,
+  },
+
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    opts = {
+      auto_install = true,
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      local lspconfig = require("lspconfig")
+
+      lspconfig.html.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.cssls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.tsserver.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.lua_ls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.clangd.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.rust_analyzer.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.jdtls.setup({
+        capabilities = capabilities,
+      })
+
+      lspconfig.pyright.setup({
+        capabilities = capabilities,
       })
     end,
   },

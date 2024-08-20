117947c018e4e00bb96a937004a69e600325174a
aspherr <huzaifa.syedd@gmail.com>
2024-08-20 20:28:43 +0100

    synced plugins

--- lua/plugins/LSP.lua
+++ lua/plugins/LSP.lua
@@ -1,33 +1,3 @@
-local function on_publish_diagnostics(_, _, params, client_id, _)
-  local uri = params.uri
-  if not uri then
-    return
-  end
-
-  local bufnr = vim.uri_to_bufnr(uri)
-
-  if not bufnr then
-    return
-  end
-
-  local diagnostics = params.diagnostics
-
-  -- Filter out the "declared package does not match the expected package" warning
-  local filtered_diagnostics = {}
-  for _, diagnostic in ipairs(diagnostics) do
-    if diagnostic.message ~= "declared package does not match the expected package" then
-      table.insert(filtered_diagnostics, diagnostic)
-    end
-  end
-
-  vim.lsp.diagnostic.save(diagnostics, bufnr, client_id)
-
-  vim.lsp.handlers["textDocument/publishDiagnostics"](nil, nil, {
-    uri = uri,
-    diagnostics = filtered_diagnostics,
-  }, client_id, nil, nil, nil)
-end
-
 return {
   {
     "williamboman/mason.nvim",
@@ -78,9 +48,6 @@ return {
 
       lspconfig.jdtls.setup({
         capabilities = capabilities,
-        on_attach = function(client, bufnr)
-          vim.lsp.handlers["textDocument/publishDiagnostics"] = on_publish_diagnostics
-        end,
       })
 
       lspconfig.pyright.setup({
